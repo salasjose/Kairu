65af2cc6680bec37eb9530c7f75039182a4838b8
jose alfonso salas rodriguez <jasr44@gmail.com>
2025-10-17 19:55:40 +0000

    corrigeme este error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="es" className="h-full">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
