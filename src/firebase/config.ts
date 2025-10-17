e55dc7a65ef7a0d19d14ed3d619d47faebb758a8
jose alfonso salas rodriguez <jasr44@gmail.com>
2025-10-17 21:44:34 +0000

    corrige los errores de la app

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-9707175342-a945e",
-  "appId": "1:85899261785:web:fbaa64e5ea7a1df997be48",
-  "apiKey": "AIzaSyB2P4izzmKm43ki8UQ3WK-SDOuNsud9zfw",
-  "authDomain": "studio-9707175342-a945e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "85899261785"
-};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -39,5 +39,3 @@ export function getSdks(firebaseApp: FirebaseApp) {
     firestore: getFirestore(firebaseApp)
   };
 }
-
-    
