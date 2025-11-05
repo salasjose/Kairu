0eaa7fece53c2a4941683eae56d286e6c315244e
jose alfonso salas rodriguez <jasr44@gmail.com>
2025-11-05 03:25:56 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-9707175342-a945e",
   "appId": "1:85899261785:web:fbaa64e5ea7a1df997be48",
+  "storageBucket": "studio-9707175342-a945e.appspot.com",
   "apiKey": "AIzaSyB2P4izzmKm43ki8UQ3WK-SDOuNsud9zfw",
-  "authDomain": "studio-9707175342-a945e.firebaseapp.com",
-  "measurementId": "",
+  "authDomain": "studio-970717534I-a945e.firebaseapp.com",
   "messagingSenderId": "85899261785"
 };
