a5aacdf0b45b3a32b7f8f87a062c347cc1eb0d63
jose alfonso salas rodriguez <jasr44@gmail.com>
2025-11-13 20:39:36 +0000

    revisa la conexion de firetore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:85899261785:web:fbaa64e5ea7a1df997be48",
   "storageBucket": "studio-9707175342-a945e.appspot.com",
   "apiKey": "AIzaSyB2P4izzmKm43ki8UQ3WK-SDOuNsud9zfw",
-  "authDomain": "studio-970717534I-a945e.firebaseapp.com",
+  "authDomain": "studio-9707175342-a945e.firebaseapp.com",
   "messagingSenderId": "85899261785"
 };
