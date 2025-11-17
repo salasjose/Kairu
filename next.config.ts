79776ef16b4a42a4cee40a175a63e66a2ad5272a
jose alfonso salas rodriguez <jasr44@gmail.com>
2025-11-17 14:20:23 +0000

    revisa el codigo  de la estacion una sale este error error-boundary-call

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
